c8b9952fac37eaf95cb40f2690862e6492e59c89
Yackeline Vilca <yackeline.vilcaes@gmail.com>
2025-09-17 00:22:49 -0500

    creacion de funcion agregarAmigo y limpiarCaja

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaNombresAmigos =[];
+
+function agregarAmigo() {
+
+    // obtener el valor del input
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    // validar que no esté vacío
+    if (nombreAmigo === "") {
+        alert("Por favor, escribe un nombre válido.");
+        return;
+    }
+
+    // guardar en el array
+    listaNombresAmigos.push(nombreAmigo);
+
+    // mostrar en label
+    let lista = document.getElementById("listaAmigos");
+    let li = document.createElement("li");
+    li.textContent = nombreAmigo;
+    lista.appendChild(li);
+    
+    // limpiar imput
+    limpiarCaja();
+
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = ''; 
+}
